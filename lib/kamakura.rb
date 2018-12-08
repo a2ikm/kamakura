5ae5f414a7657b9a92e4d5e741449b60770c621b
Masato Ikeda <masato.ikeda@gmail.com>
2018-12-08 23:07:58 +0900

    Symbolize attributes' keys

--- lib/kamakura.rb
+++ lib/kamakura.rb
@@ -2,10 +2,19 @@ require "kamakura/version"
 
 module Kamakura
   def initialize(attributes = {})
-    @__attributes = attributes.dup.freeze
+    @__attributes = symbolize_keys(attributes).freeze
   end
 
   def attributes
     @__attributes
+  end
+
+  private
+
+  def symbolize_keys(hash)
+    hash.inject({}) do |s, (k, v)|
+      s[k.to_sym] = v
+      s
+    end
   end
 end
--- test/kamakura_test.rb
+++ test/kamakura_test.rb
@@ -9,4 +9,9 @@ class KamakuraTest < Minitest::Test
     user = User.new(:name => "Piotr", :age => 31)
     assert_equal({ :name => "Piotr", :age => 31 }, user.attributes)
   end
+
+  def test_attributes_with_string_keys
+    user = User.new("name" => "Piotr", "age" => 31)
+    assert_equal({ :name => "Piotr", :age => 31 }, user.attributes)
+  end
 end
