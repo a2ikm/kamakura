02359c6ba773fb1d607b3af662470d2d78bf4e46
Masato Ikeda <masato.ikeda@gmail.com>
2018-12-13 02:10:38 +0900

    Cosmetic

--- test/kamakura/value_type_test.rb
+++ test/kamakura/value_type_test.rb
@@ -2,40 +2,40 @@ require "test_helper"
 
 class KamakuraValueTypeTest < Minitest::Test
   def test_string_parse
-    assert_equal "abcd", lookup_value_type(:string).parse("abcd")
-    assert_nil lookup_value_type(:string).parse(nil)
-    assert_equal "1234", lookup_value_type(:string).parse(1234)
+    string = Kamakura.lookup_value_type(:string)
+
+    assert_nil string.parse(nil)
+    assert_equal "abcd", string.parse("abcd")
+    assert_equal "1234", string.parse(1234)
   end
 
   def test_integer_parse
-    assert_nil lookup_value_type(:integer).parse("abcd")
-    assert_nil lookup_value_type(:integer).parse(nil)
-    assert_equal 1234, lookup_value_type(:integer).parse("1234")
+    integer = Kamakura.lookup_value_type(:integer)
+
+    assert_nil integer.parse("abcd")
+    assert_nil integer.parse(nil)
+    assert_equal 1234, integer.parse("1234")
   end
 
   def test_boolean_parse
-    assert_nil lookup_value_type(:boolean).parse(nil)
+    boolean = Kamakura.lookup_value_type(:boolean)
 
-    assert_equal true, lookup_value_type(:boolean).parse(true)
-    assert_equal true, lookup_value_type(:boolean).parse("true")
-    assert_equal true, lookup_value_type(:boolean).parse(1)
-    assert_equal true, lookup_value_type(:boolean).parse("1")
-
-    assert_equal false, lookup_value_type(:boolean).parse(false)
-    assert_equal false, lookup_value_type(:boolean).parse("false")
-    assert_equal false, lookup_value_type(:boolean).parse(0)
-    assert_equal false, lookup_value_type(:boolean).parse("0")
+    assert_nil boolean.parse(nil)
+    assert_equal true, boolean.parse(true)
+    assert_equal true, boolean.parse("true")
+    assert_equal true, boolean.parse(1)
+    assert_equal true, boolean.parse("1")
+    assert_equal false, boolean.parse(false)
+    assert_equal false, boolean.parse("false")
+    assert_equal false, boolean.parse(0)
+    assert_equal false, boolean.parse("0")
   end
 
   def test_time_parse
-    assert_nil lookup_value_type(:time).parse(nil)
-    assert_equal Time.at(1), lookup_value_type(:time).parse(1)
-    assert_equal Time.at(1), lookup_value_type(:time).parse("1")
-  end
+    time = Kamakura.lookup_value_type(:time)
 
-  private
-
-  def lookup_value_type(name)
-    Kamakura.lookup_value_type(name)
+    assert_nil time.parse(nil)
+    assert_equal Time.at(1), time.parse(1)
+    assert_equal Time.at(1), time.parse("1")
   end
 end
