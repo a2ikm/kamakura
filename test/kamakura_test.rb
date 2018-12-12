a72031e44cdb178e7d23def1ab99d55c66ccae06
Masato Ikeda <masato.ikeda@gmail.com>
2018-12-13 02:45:26 +0900

    Cosmetic

--- test/kamakura_test.rb
+++ test/kamakura_test.rb
@@ -19,18 +19,27 @@ class KamakuraTest < Minitest::Test
     attribute :card_number, :integer, key: :cardNumber
   end
 
+  USER_ATTRIBUTES = {
+    name:       "Piotr",
+    age:        31,
+    active:     true,
+    friend_ids: [1, 2],
+  }
+
   def test_attributes
-    user = User.new(:name => "Piotr", :age => 31, :active => true, :friend_ids => [1, 2])
-    assert_equal({ :name => "Piotr", :age => 31, :active => true, :friend_ids => [1, 2] }, user.attributes)
+    user = User.new(USER_ATTRIBUTES)
+    assert_equal USER_ATTRIBUTES, user.attributes
   end
 
   def test_attributes_with_string_keys
-    user = User.new("name" => "Piotr", "age" => 31, "active" => true, "friend_ids" => [1, 2])
-    assert_equal({ :name => "Piotr", :age => 31, :active => true, :friend_ids => [1, 2] }, user.attributes)
+    sym_attributes = USER_ATTRIBUTES.inject({}) { |s, (k, v)| s[k.to_s] = v; s }
+
+    user = User.new(sym_attributes)
+    assert_equal USER_ATTRIBUTES, user.attributes
   end
 
   def test_hash_like_accessor_with_symbol_argument
-    user = User.new(:name => "Piotr", :age => 31, :active => true, :friend_ids => [1, 2])
+    user = User.new(USER_ATTRIBUTES)
     assert_equal "Piotr", user[:name]
     assert_equal 31, user[:age]
     assert_equal true, user[:active]
@@ -38,7 +47,7 @@ class KamakuraTest < Minitest::Test
   end
 
   def test_hash_like_accessor_with_string_argument
-    user = User.new("name" => "Piotr", "age" => 31, :active => true, :friend_ids => [1, 2])
+    user = User.new(USER_ATTRIBUTES)
     assert_equal "Piotr", user["name"]
     assert_equal 31, user["age"]
     assert_equal true, user["active"]
@@ -46,7 +55,7 @@ class KamakuraTest < Minitest::Test
   end
 
   def test_attribute_accessor
-    user = User.new(:name => "Piotr", :age => 31, :active => true, :friend_ids => [1, 2])
+    user = User.new(USER_ATTRIBUTES)
     assert_equal "Piotr", user.name
     assert_equal 31, user.age
     assert_equal true, user.active
@@ -54,7 +63,7 @@ class KamakuraTest < Minitest::Test
   end
 
   def test_type_conversion
-    user = User.new(:name => "Piotr", :age => "31", :active => true, :friend_ids => [1, "2"])
+    user = User.new(USER_ATTRIBUTES)
     assert_equal "Piotr", user.name
     assert_equal 31, user.age
     assert_equal true, user.active
